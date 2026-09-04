8c72e7c262f7323d5e3abf4d9ba514491d4055e7
josephotaala <otaalajose2002@gmail.com>
2026-09-04 01:15:32 -0700

    Delete Isore directory

--- Isore/Osore/Home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Announcement, Notification, Photo, Profile
-
-admin.site.register(Profile)
-admin.site.register(Announcement)
-admin.site.register(Notification)
-admin.site.register(Photo)
